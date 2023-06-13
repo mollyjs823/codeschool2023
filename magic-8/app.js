82b17cc47ee080b0d323b4447898d973d19076c7
mollyjs823 <mollyjs@hawaii.edu>
2023-06-13 12:14:28 -0600

    magic 8 updated

--- magic-8/app.js
+++ magic-8/app.js
@@ -10,21 +10,33 @@ Vue.createApp({
                 "Probably",
                 "Probably Not"
             ],
-            question: ""
+            question: "",
+            history: [
+            ]
         }
     },
     methods : {
         askQuestion: function() {
-            console.log(this.question);
-
             if (!this.isValidQuestion()) return;
-
+            
             let index = Math.floor(Math.random() * this.messageBank.length);
             this.message = this.messageBank[index];
+            
+            this.history.push({
+                question: this.question,
+                answer: this.message
+            });
+
+            console.log(this.history);
+            
             this.question = "";
         },
         isValidQuestion: function() {
             return this.question[this.question.length - 1] == '?';
+        },
+        deleteItem: function(item) {
+            var index = this.history.indexOf(item);
+            this.history.splice(index, 1);
         }
     }
 }).mount("#app");
