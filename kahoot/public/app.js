f3de47f0f2d8c283399182090a2d197a7de7367b
mollyjs823 <mollyjs@hawaii.edu>
2023-07-05 12:09:48 -0600

    kahoot

--- kahoot/public/app.js
+++ kahoot/public/app.js
@@ -12,6 +12,21 @@ Vue.createApp({
         }
     },
     methods : {
+        loggedIn: function() {
+            var options = {
+                credentials: "include"
+            };
+            
+            fetch(URL + "session", options)
+            .then(response => response.json())
+            .then(data => {
+                if (data && data.cookie && data.userId) {
+                    this.page = "";
+                } else {
+                    this.page = "auth";
+                }
+            });
+        },
         signup: function() {
             var myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
@@ -26,16 +41,47 @@ Vue.createApp({
             .then((response) => {
                 if (response.status === 201) {
                     this.page = '';
-                    // this.createSession();
+                    this.createSession();
                 } else {
                     response.text().then((data) => {
                         alert(data);
-                    })
-                    // alert("Unable to create user");
+                    });
                 }
-            })
+            });
+        },
+        createSession: function() {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var options = {
+                method: "POST",
+                headers: myHeaders,
+                body: JSON.stringify(this.user),
+                credentials: 'include'
+            };
+
+            fetch(URL + "session", options)
+            .then((response) => {
+                if (response.status === 201) {
+                    response.text().then((data) => {
+                        console.log(data);
+                        if (data) {
+                            data = JSON.parse(data);
+                            this.page = "";
+                            this.user.name = data.name;
+                        } else {
+                            alert("Can't log in");
+                        }
+                    });
+                } else {
+                    response.text().then((data) => {
+                        alert(data);
+                    });
+                }
+            });
         }
     },
     created : function() {
+        this.loggedIn();
     }
 }).mount("#app");
